456dc50b2f3c46ab66421ac36b045bfc4b5a02f9
Joseph Chambers <joseph@michael-chambers.com>
2017-11-21 16:53:55 -0700

    Updating State from the reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,7 +4,16 @@
 // 2. copy of current state
 
 export default function posts (state = [], action) {
-	console.log('This post will change!')
-	console.log(state, action)
+	switch(action.type) {
+		case 'INCREMENT_LIKES':
+		const i = action.index
+			return [
+				...state.slice(0, i), // before the one we are updating
+				{...state[i], likes: state[i].likes + 1},
+				...state.slice(i + 1) // after the one we are updating
+			]
+		default:
+			return state;
+	}
 	return state;
 }
