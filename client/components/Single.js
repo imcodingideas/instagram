a2e34ad40768700db46da6700a97292ec9a85d35
Joseph Chambers <joseph@michael-chambers.com>
2017-11-21 19:36:23 -0700

    Small Change. Testing GPG

--- client/components/Single.js
+++ client/components/Single.js
@@ -7,7 +7,7 @@ export default class Single extends Component {
 	render() {
 		const { postId } = this.props.params
 
-		const i = this.props.posts.findIndex((post) => post.code === postId)
+		const i = this.props.posts.findIndex(post => post.code === postId)
 		const post = this.props.posts[i]
 
 		const postComments = this.props.comments[postId] || []
